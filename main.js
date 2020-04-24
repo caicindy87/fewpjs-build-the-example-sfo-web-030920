24621f07d5503d579c2488bcadcf06254fdaec58
caicindy87 <caicindy87@gmail.com>
2020-04-23 17:24:28 -0700

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,45 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  listenToHeart();
+}) 
 
+const errorModal = document.getElementById('modal')
+errorModal.className = 'hidden'
 
+function listenToHeart() {
+  const body = document.querySelector('body');
+  body.addEventListener('click', function(event) {
+    if (event.target.tagName === "SPAN") {
+      const heart = event.target
+      mimicServerCall()
+        .then(resp => {
+          if (heart.innerText === FULL_HEART) {
+            heart.className = ''
+            heart.innerText = EMPTY_HEART
+          } else {
+          heart.className = 'activated-heart'
+          heart.innerText = FULL_HEART
+          }
+        })
+        .catch((error) => renderError(error))
+    }
+  })
+}
+
+function renderError(error) {
+  errorModal.className = '';
+   const modalMsg = document.getElementById('modal-message');
+   modalMsg.innerText = error;
+  hideModal();
+}
+
+function hideModal() {
+  setTimeout(() => {
+    errorModal.className = 'hidden'
+  }, 5000)
+}
 
 
 //------------------------------------------------------------------------------
